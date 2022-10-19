19b5461547f83c42622da31ef9238a140b0b2cfa
최지영 <cwd05011@gmail.com>
2022-10-20 00:24:01 +0900

    set basename

--- src/app.jsx
+++ src/app.jsx
@@ -7,7 +7,7 @@ import PrivateRoute from "./routes/private_route";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
